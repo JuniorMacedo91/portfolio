bc2dc548ac7ce4a7cfb3a096bc191e396effbef1
unknown <devfrontend01@gmail.com>
2021-11-23 14:58:45 -0300

    Menu burger added

---
+++ js/home.js
@@ -0,0 +1,16 @@
+const menuBurger = document.querySelector('.menu-btn')
+const navbar = document.querySelector('.navbar')
+
+let menuToggle = false
+
+menuBurger.addEventListener('click', ()=>{
+    if(!menuToggle){
+        menuBurger.classList.add('open')
+        navbar.style.width = '350px'
+        menuToggle = true
+    } else{
+        menuBurger.classList.remove('open')
+        navbar.style.width = '0'
+        menuToggle = false
+    }
+})
