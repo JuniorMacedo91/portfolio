5b495fb7c84c56a0f971c9a9f71a51ba1a41ba83
unknown <devfrontend01@gmail.com>
2021-11-26 23:21:07 -0300

    closeMenu function created

--- js/home.js
+++ js/home.js
@@ -3,18 +3,22 @@ const navbar = document.querySelector('.navbar')
 
 let menuToggle = false
 
-menuBurger.addEventListener('click', ()=>{
+function openMenu(){
     if(!menuToggle){
         menuBurger.classList.add('open')
-        navbar.style.width = '350px'
+        navbar.style.width = '100%'
         menuToggle = true
-    } else{
+    } 
+    else{
         menuBurger.classList.remove('open')
         navbar.style.width = '0'
         menuToggle = false
     }
-})
+}
 
-navbar.addEventListener('click', ()=>{
+function closeMenu(){
     navbar.style.width = '0'
-})
+}
+
+menuBurger.addEventListener('click', openMenu)
+navbar.addEventListener('click', closeMenu)
